95624fcf414fb22d13500e835db746b72b83ccf5
1-8192 <allegranzia@gmail.com>
2019-04-03 11:41:39 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const obj = { "method": "POST", "body": {
+  "firstName": "Tom", "registryMessage": "whatever"}}
+
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", obj)
+  .then(function(response) {
+    return response.json()
+  })
+    .then(function(parsedJson) {
+      return parsedJson.message
+      })
+  }
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", obj)
+  .then(function(response) {
+    return response.json()
+  })
+    .then(function(parsedJson) {
+      return parsedJson.message
+      })
+      .catch(function(error) {
+        return ":("
+      })
+}
