d7398c8a0853b3e08a8267d61b0cd3f9069d2a74
cstruong <christophertruongm@gmail.com>
2019-04-03 19:36:42 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let formObj = {
+  "firstName": "Chrees",
+  "registryMessage": "AHHHHHH"
+};
+
+let formThing = {
+  method: "POST",
+  body: formObj
+};
+
+function registerSelf() {
+ return fetch('http://guestbook.example.com/register', formThing)
+  .then(response => {
+    debugger
+    return response.json();
+  })
+  .then(jsonPromise => {
+    return jsonPromise.message;
+  })
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', formThing)
+  .then(response => {
+    return response.json();
+  })
+  .then(jsonPromise => {
+    return jsonPromise.message;
+  })
+  .catch(error => {
+    return ":("
+  })
+}
