b9040fae610d33b50469bdcf80698bfce8f307af
Simon Mei <smei8800@gmail.com>
2019-04-03 20:54:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+configObj = {};
+
+function registerSelf() {
+  let formData = { firstName: "Nomis" };
+  let configObj = {
+    method: "POST",
+    body: formData
+  };
+  return fetch("http://guestbook.example.com/register", configObj)
+    .then(response => response.json())
+    .then(content => {
+      return content.message;
+    });
+}
+
+function errorSelf() {
+  let formData = { firstName: "Nomis" };
+  let configObj = {
+    method: "POST",
+    body: formData
+  };
+  return fetch("http://guestbook.example.com/register", configObj)
+    .then(response => response.json())
+    .then(content => {
+      if (response == "OK") {
+        console.log(response);
+        return content.message;
+      }
+    })
+    .catch(function(x){
+      return ":("
+    });
 }
