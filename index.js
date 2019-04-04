6300a713480252576e035cd9cc56a49b8c1887d0
Rhohoman <rhohoman@gmail.com>
2019-04-04 12:15:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {"method": "POST", "body": {"firstName": "BYRONIUS KARBITUS MARIS","registryMessage": "SALVETE AMICI!"}})
+  .then(response => response.json())
+  .then(json => json.message)
 }
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {"method": "POST", "body": {"firstName": "ME", "registryMessage": "YAYO!"}})
+  .then(response => response.json())
+  .then(json => json.message)
+  .catch( function(anything) {
+    return ":(";
+  });
+}
+
+
+//makes a request to oregister with a name and a message
+
+//processes the returned JSON and extracts the value of the 'message' key
+
+//processes a bad request and returns a sad face:
