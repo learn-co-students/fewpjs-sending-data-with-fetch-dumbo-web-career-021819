e986cb381b5fffb54661dd935d1d19497c9d84d8
campbelllssoup <matjocampbell@gmail.com>
2019-04-07 14:25:39 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,43 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: {
+      'firstName': 'Matthew',
+      'registryMessage': 'Hello World!'
+    }
+  }).then( (response) => {
+    return response.json();
+  })
+  .then(message => {
+     return message.message;
+  });
+};
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: {
+      'firstName': 'Matthew',
+      'registryMessage': 'Hello World!'
+    }
+  }).then( (response) => {
+    return response.json();
+  })
+  .then( message => {
+    return message.message;
+  }).catch( error => {
+    return ":(";
+  });
+};
